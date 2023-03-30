7c9e7a0ee6c1c658364b3b4bc364d873f1385d27
raccoon-control-DSS <129355062+raccoon-control-DSS@users.noreply.github.com>
2023-03-30 13:08:00 +0900

    Update code_ui.R

--- code_ui.R
+++ code_ui.R
@@ -15,110 +15,95 @@ library(scales)
 library(showtext)
 
 
-shinyServer(function(input, output) {
+# Define UI for application
+shinyUI(fluidPage(
   
-  #CPUEの算出
-  CPUE <- reactive({
-    return(input$capture / input$effort * 100)
-  })
+  # Application title
+  titlePanel("アライグマ防除に必要な予算策定支援システム"),
   
-  #推定個体数
-  POPU <- reactive({
-    return(input$area * {(CPUE() + 0.6924) / 1.6045})
-  })
+  # Inputbox
+  sidebarLayout(
+    
+    sidebarPanel(
+      
+      fluidRow(
+        h4("【根絶可能性に関するチェックリスト】"),
   
-  #捕獲目標数
-  GOAL <- reactive({
-    return(as.numeric(input$percent) * POPU())
-  })
-  
-  #必要TN数
-  TRNI <- reactive({
-    return((GOAL() / CPUE()) *100)
-  })
-  
-  #必要わなかけ日数
-  DAYS <- reactive({
-    return(TRNI() / input$trapd)
-  })
-  
-  #必要人件費
-  SALA <- reactive({
-    return(input$salary * input$worker * DAYS())
-  })
-  
-  #わな購入費
-  TRAP <- reactive({
-    return(input$purchase * 13000)
-  })
-  
-  #餌購入費
-  BAIT <- reactive({
-    return(input$bait * TRNI())
-  })
-  
-  #車リース代
-  CARS <- reactive({
-    return(input$car * DAYS() * 2400)
-  })
-  
-  #殺処分費用
-  EUTH <- reactive({
-    return(input$euthanasia * GOAL())
-  })
-  
-  #必要総コスト
-  SUMM <- reactive({
-    return(SALA() + TRAP() + BAIT() + CARS() + EUTH())
-  })
-
-  
-  #FS結果
-  FS <- reactive({
-    return(length(input$FSrequired))
-  })
-  
-  
-  #output_FStext
-  output$FStext <- renderText({
-    paste("【根絶目標の設定について】対象地域でのアライグマ根絶は", 
-          if(FS() == 4) {print("可能です。")}
-          else{print("不可能です。根絶を目指すにはチェックリストの必須検討条件をすべて満たしてください。")
-          })
-  })
-  
-  #output_text
-  output$text <- renderText({
-    paste("【計算結果】",
-          '\n',
-          "地域CPUE値:", format(round(CPUE(), 2), nsmall = 2),
-          '\n',
-          "対象地域推定生息頭数:", format(round(POPU(), 2), nsmall = 2),"頭",
-          '\n',
-          "生息頭数減少に必要な捕獲頭数:", format(round(GOAL(), 2), nsmall = 2),"頭",
-          '\n',
-          "必要わなかけ日数:", format(round(DAYS(), 2), nsmall = 2),"日",
-          '\n', 
-          "アライグマ対策にかかる総コスト:", format(round(SUMM(), 2), nsmall = 2), "円"
-    )
-  })
-  
-  
-  #output_graph
-  output$plot <- renderPlot({
-    costs <- c("総コスト", "総コスト", "総コスト", "総コスト", "総コスト")
-    lists <- c("人件費", "箱わな購入費", "誘引餌購入費", "車リース費", "殺処分費")
-    total <- c(round(SALA(), digits = 2), round(TRAP(), digits =2),
-               round(BAIT(), digits = 2), round(CARS(), digits = 2),
-               round(EUTH(), digits =2))
-    data <- data.frame(costs, lists, total)
-    g <- ggplot(data, aes(x = costs, y = total, fill = lists))
-    g <- g + labs(x = " ", y = "コストの内訳（%）", fill = "凡例")
-    g <- g + geom_bar(stat = "identity", position = "fill")
-    g <- g + scale_y_continuous(labels = percent)
-    g <- g + coord_flip()
-    g <- g + scale_fill_nejm()
-    plot(g)
-  })
-  
-})
+        checkboxGroupInput("FSrequired", label = "必須検討条件",
+                           choices = list("再侵入の可能性がゼロである" = 1,
+                                          "防除手法がすべての個体に有効である" = 1,
+                                          "貴重な種や群集への悪影響がない" = 1,
+                                          "防除事業が増加率（繁殖率・新規個体侵入率）を上回る" = 1)),
+        
+        checkboxGroupInput("FSadd", label = "付加検討条件",
+                           choices = list("適切な社会・政治環境が整っている" = 1,
+                                          "低密度でも生存個体の探索が可能である" = 1,
+                                          "cost-benefit分析はコントロールよりも根絶に有効である" = 1,
+                                          "気候的制約" = 1,
+                                          "全ての個体に合法的である" = 1)),
+        
+        br(),
+        
+        h4("【対象地域の捕獲実績】"),
+        numericInput("effort",
+                     label = "わな日（対象地域に設置したわな基数 × 設置日数）",
+                     min = 0, value = 1000),
+        helpText("※わな日が正確に把握されていない場合、
+                       このシステムでは必要な予算を正しく計算することができません"),
+        numericInput("capture",
+                     label = "対象地域で捕獲されたアライグマ頭数（頭）",
+                     min = 0, value = 45),
+        numericInput("area",
+                     label = "対象地域の面積（km2）",
+                     min = 0, value = 747.6),
+        
+        br(),
+        
+        h4("【目標設定】"),
+        radioButtons("percent",
+                     label = "対象地域の捕獲頭数に関する目標",
+                     choices = list("推定生息頭数の50%を捕獲" = 0.5,
+                                    "推定生息頭数の60%を捕獲" = 0.6,
+                                    "推定生息頭数の70%を捕獲" = 0.7),
+                     selected = 0.6),
+        
+        
+        br(),
+        
+        h4("【投入可能な資源及び努力量】"),
+        numericInput("trapd",
+                     label = "1日のわな見回り基数（基/日）",
+                     min = 0, value = 180),
+        numericInput("worker",
+                     label = "捕獲作業員数（人）",
+                     min = 0, value = 18),
+        numericInput("car",
+                     label = "必要な車の台数（台）",
+                     min = 0, value = 9),
+        
+        br(),
+        
+        h4("【必要物品数及び単価】"),
+        numericInput("salary",
+                     label = "捕獲作業員の作業単価（円/日）",
+                     min = 0, value = 4000),
+        numericInput("purchase",
+                     label = "箱わな購入基数（予備を含む）",
+                     min = 0, value = 270),
+        numericInput("bait",
+                     label = "誘引餌の単価（円/基）",
+                     min = 0, value = 40),
+        numericInput("euthanasia",
+                     label = "アライグマの殺処分単価（円/頭）",
+                     min = 0, value = 8000)
+      )
+    ),
+    
+    mainPanel(
+      textOutput("FStext"),
+      verbatimTextOutput("text"),
+      plotOutput("plot")
+    ),
+    
+    position = "left", fluid =TRUE)
+))
